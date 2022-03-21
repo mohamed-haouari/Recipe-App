be2c61bc457593079f4ac23c639cb73eae65fd65
medhaouari7 <medhaouari025@gmail.com>
2022-03-21 09:00:20 +0100

    search work!!

--- recipe-app/src/components/Search.jsx
+++ recipe-app/src/components/Search.jsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
 
 function Search() {
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
   const submitHandler = (e) => {
   e.preventDefault();
+  navigate('/searched/'+input)
   }; 
-// 1:36:46
+
   return (
     <FormStyle onSubmit={submitHandler} >
       <div>
